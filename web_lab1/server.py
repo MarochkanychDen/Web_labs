1a68cbd6f80e5ac810adc713e883d289c04fdb04
MarochkanychDen <79100376+MarochkanychDen@users.noreply.github.com>
2021-02-22 18:32:45 +0200

    lab_1 2.0

--- web_lab1/server.py
+++ web_lab1/server.py
@@ -1,4 +1,5 @@
 from socket import socket, AF_INET, SOCK_STREAM, SO_RCVBUF, SOL_SOCKET
+from threading import Thread
 
 HOST = "localhost"
 PORT = 1234
@@ -10,10 +11,12 @@ def read_from_socket(s: socket) -> bytearray:
     data = s.recv(buff_size)
     return data
 
-def calculation(num: bytearray)->bytearray:
+def calculation(num: bytearray,client_socket:socket):
     new_num=num.decode(CODING)
     res=str(int(new_num)**2)
-    return res
+    client_socket.send(res.encode(CODING))
+
+
 
 def main() -> None:
     sock = socket(AF_INET, SOCK_STREAM)
@@ -22,12 +25,12 @@ def main() -> None:
     print("Ready, Daddy")
     while True:
         (client_socket, client_info) = sock.accept()
-
         raw_payload = read_from_socket(client_socket)
-        result =calculation(raw_payload)
-        client_socket.send(result.encode(CODING))
-        client_socket.close()
 
+        potok =Thread(target= calculation,args=(raw_payload,client_socket))
+        potok.start()
+        potok.join()
+        client_socket.close()
 
 if __name__ == '__main__':
     main()

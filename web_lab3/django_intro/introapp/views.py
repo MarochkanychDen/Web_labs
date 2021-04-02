4fd528d4ef5dc6856210533dbb622c87e7873874
MarochkanychDen <79100376+MarochkanychDen@users.noreply.github.com>
2021-04-02 10:18:45 +0300

    web_lab3

--- web_lab3/django_intro/introapp/views.py
+++ web_lab3/django_intro/introapp/views.py
@@ -22,26 +22,17 @@ class ExampleView(APIView):
         Описує обробник POST запитів для даної точки доступу. Аналогічно можна визначити
         обробники і для інших HTTP методів.
         """
-        # Десеріалізувати тіло запиту із JSON строки у словник. Зауважте, тут ми вже маємо
-        # доступ до усього тіла запиту, тобто Django гарантує, що усе тіло буде повінстю зчитане
-        # незалежно від розміру.
         parsed_request = loads(request.body)
-        # Створити клас-серіалізатор, що провалідує правильність заповнення полів об'єкту
         request_data_serializer = CalculationRequestSerializer(data=parsed_request)
         if not request_data_serializer.is_valid():
-            # Якщо валідацію не пройдено - сповістити клієнта про неправильний формат запиту
             return Response(status=400)
-        # Створити об'єкт, що описує дані запиту, із провалідованих даних
         request_data = CalculateRequest(**request_data_serializer.validated_data)
-        # Використання даних запиту у бізнес-логіці
         calculation_result = Calculator.calculate(request_data.input_value)
-        # Створити об'єкт, що описує дані відповіді на запит
         response_data = CalculateResponse(calculation_result)
-        # Створити клас-серіалізатор, що підготує об'єкт з даними для відповіді до серіалізації
         response_data_serializer = CalculationResponseSerializer(response_data)
-        # Створити об'єкт відповіді із даними
+        if not response_data_serializer.is_valid():
+            return Response(status=500)
         response = Response(response_data_serializer.data)
-        # Повернути відповідь. Коректна доставка гарантована фреймворком.
         return response
 
 

3a921fc09b96e206fb749d89c716afaa48ca6a9e
MarochkanychDen <79100376+MarochkanychDen@users.noreply.github.com>
2021-04-21 00:38:10 +0300

    Delete laba_4(kontrol) directory

--- laba_4(kontrol)/Server/cyberspryt/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import teams
-
-admin.site.register(teams)
--- laba_4(kontrol)/Server/cyberspryt/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import teams
-
-class SerializerData(serializers.ModelSerializer):
-    id = serializers.IntegerField(required=False)
-
-    class Meta:
-        model = teams
-        fields = '__all__'
